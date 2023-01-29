22bb7503d1ebe2a9113fe49363343acb78002e71
Dibya Jyoti Sarmah <dibyajyotisarmah531@gmail.com>
2023-01-29 19:51:03 +0530

    Data getting stored in DB from registration page

---
+++ Signin_Page_JDBC/src/main/java/com/signin/registration/RegistrationServlet.java
@@ -0,0 +1,74 @@
+package com.signin.registration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String uname  = request.getParameter("name") ;
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
+		Connection con = null;
+		RequestDispatcher dispatcher = null ;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/signin","root","3183");
+			
+			PreparedStatement pst = con.prepareStatement("insert into users(uname,upwd, uemail,umobile) values(?,?,?,?)");
+			
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if (rowCount > 0 ) {
+				request.setAttribute("status", "success");
+				response.sendRedirect("registration.jsp");
+			}else {
+				request.setAttribute("status", "failed");
+			}
+		
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
